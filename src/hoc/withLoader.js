d9dbb7c38707a5b80f48ee4df344e810c4c68d50
BartekB94 <wurir1994@gmail.com>
2024-10-23 16:39:45 +0200

    refactor HOC to arrow fn

--- src/hoc/withLoader.js
+++ src/hoc/withLoader.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const withLoader = function (message) {
-  return function (WrappedComponent) {
-    return class extends React.Component {
-      render() {
-        const { isLoaded } = this.props;
-        if (isLoaded) {
-          return <WrappedComponent {...this.props} />;
-        }
-        return <div>{message}</div>;
+const withLoader = (message) => (WrappedComponent) => {
+  return class extends React.Component {
+    render() {
+      const { isLoaded } = this.props;
+      if (isLoaded) {
+        return <WrappedComponent {...this.props} />;
       }
-    };
+      return <div>{message}</div>;
+    }
   };
 };
+
 
 export default withLoader;
